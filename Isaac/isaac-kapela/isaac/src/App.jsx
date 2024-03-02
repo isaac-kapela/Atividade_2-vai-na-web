ebf5f9f1f73352265decbbc01740ecf2a82ee066
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-03-01 22:24:01 -0300

    subindo o projeto

---
+++ Isaac/isaac-kapela/isaac/src/App.jsx
@@ -0,0 +1,17 @@
+import Login from "./components/registro/registro";
+
+import './App.sass'
+
+function App() {
+  
+  return (
+    <>
+    <div className="app-container">
+      <Login/>
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ Isaac/isaac-kapela/isaac/src/components/registro/registro.jsx
@@ -0,0 +1,27 @@
+// import registro from "./registrocss";
+import AddAvatar from "../../assets/img/add avatar.png";
+import '../../App'
+
+
+const registro = () => {
+    return (
+        <div className="registro-container">
+            <span className="titulo">plus chat</span>
+            <span className="subTitulo">Registro</span>
+            <form>
+                <input type="text" placeholder="Digite o seu nome" />
+                <input type="text" placeholder="Digite o seu email" />
+                <input type="password" placeholder="Digite a sua senha" />
+                <input type="file" id="file"  style={{dispaly:'none' }}/>
+                <label htmlFor="file">
+                    <img src={AddAvatar} alt="" />
+                    <span>Adiciona o avatar</span>
+                </label>
+                <button>registrar</button>
+            </form>
+            <p>Você ainda não se registrou? Resgistar</p>
+        </div>
+    )
+}
+
+export default registro;
